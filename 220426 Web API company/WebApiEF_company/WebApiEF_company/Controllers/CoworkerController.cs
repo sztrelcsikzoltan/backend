6a9839bff4788a62c9d7f7542eff0f16c9698a8f
zoltan <sztrelcsikz@kkszki.hu>
2022-04-26 08:24:22 +0200

    220426 Web API company

---
+++ 220426 Web API company/WebApiEF_company/WebApiEF_company/Controllers/CoworkerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using WebApiEF_company.Models;
+using WebApiEF_company.Services;
+
+namespace WebApiEF_company.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoworkerController : ControllerBase
+    {
+        private readonly CoworkerService service;
+        public CoworkerController(CoworkerService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        [Route("/GetCoworkerByEmail")]
+        public Coworker GetCoworkerByEmail([Required] string email)
+        {
+            return service.GetCoworkerByEmail(email);
+        }
+        
+        [HttpGet]
+        [Route("/GetCoworkerNumber")]
+        public int GetCoworkerNumber()
+        {
+            return service.GetCoworkerNumber();
+        }
+    }
+}
---
+++ 220426 Web API company/WebApiEF_company/WebApiEF_company/Services/CoworkerService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using WebApiEF_company.Models;
+
+namespace WebApiEF_company.Services
+{
+    public class CoworkerService
+    {
+        private readonly companyContext context;
+        public CoworkerService(companyContext context)
+        {
+            this.context = context;
+        }
+
+        public Coworker GetCoworkerByEmail(string email)
+        {
+            return context.Coworkers.Where(c => c.Email == email).Include(c => c.Notebooks).Include(c => c.Phones).FirstOrDefault();
+                
+        }
+
+        public int GetCoworkerNumber()
+        {
+            return context.Coworkers.Count();
+        }
+    }
+}
